1f6757bada8313e87cf75aa7b8dfa13dbf47786b
cwinters8 <clwinters8@gmail.com>
2019-01-27 20:33:15 -0800

    Pug!

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+
 // create the app
 const app = express();
+app.set('view engine', 'pug');
 
-// set up a route
-app.get('/', (request, response) => {
-    response.send("I love treehouse!");
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/cards', (req, res) => {
+    res.render('card', {
+        prompt: "Who is buried in Grant's tomb?",
+        // hint: "Think about whose tomb it is."
+    });
 });
 
 // start the app
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!');
+});
