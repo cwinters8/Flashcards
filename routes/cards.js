058a7f380c29cad24146701d4394a8cc5c9667ef
cwinters8 <clwinters8@gmail.com>
2019-01-29 18:45:29 -0800

    add questions and answers

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const {data} = require('../data/flashcardData.json');
+const {cards} = data;
 
-router.get('/', (req, res) => {
-    res.render('card', {prompt: "Who is buried in Grant's tomb?"});
+router.get('/:id', (req, res) => {
+    const {side} = req.query;
+    const {id} = req.params;
+    const text = cards[id][side];
+    const {hint} = cards[id];
+
+    const templateData = {text, id};
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.otherSide = 'answer';
+    } else if (side === 'answer') {
+        templateData.otherSide = 'question';
+    }
+
+    res.render('card', templateData);
 });
 
 module.exports = router;
