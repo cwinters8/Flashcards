24f93f896be65464b9a355264b19163c37bbed8d
cwinters8 <clwinters8@gmail.com>
2019-01-29 17:24:31 -0800

    error handling and modularizing

--- app.js
+++ app.js
@@ -10,49 +10,25 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-// middleware examples
+// import routes
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+// use routes
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+// 404 handler
 app.use((req, res, next) => {
-    console.log("Hello");
-    next();
-});
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
 
-app.use((req, res, next) => {
-    console.log("World");
-    next();
-});
-
-
-// routes
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', {name});
-    } else {
-        res.redirect('/hello');
-    }
-});
-app.get('/cards', (req, res) => {
-    res.render('card', {
-        prompt: "Who is buried in Grant's tomb?",
-        hint: "Think about whose tomb it is."
-    });
-});
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-// post routes
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
+// error middleware
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 // start the app
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// routes
+router.get('/', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
+});
+router.get('/hello', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+});
+// post routes
+router.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
+router.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+});
+
+module.exports = router;
